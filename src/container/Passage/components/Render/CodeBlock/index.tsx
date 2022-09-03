44f9d5fdd6e7a30566c5d1e6fdb740d746f673df
zhongbr <1178395080@qq.com>
2022-09-03 21:49:01 +0800

    feat: add code block component

--- src/container/Passage/components/Render/CodeBlock/index.tsx
+++ src/container/Passage/components/Render/CodeBlock/index.tsx
@@ -3,10 +3,11 @@
  * @Author: 张盼宏
  * @Date: 2022-08-29 22:46:04
  * @LastEditors: 张盼宏
- * @LastEditTime: 2022-09-03 18:53:24
+ * @LastEditTime: 2022-09-03 21:48:43
  */
 import React from 'react';
 import Paper from "@mui/material/Paper";
+import { CopyBlock, github } from 'react-code-blocks';
 
 import { IBaseProps } from '@/types/markdown';
 import styles from './style.module.less';
@@ -16,7 +17,12 @@ const CodeBlock: React.FC<IBaseProps> = (props) => {
 
     return (
         <Paper id={node.key} className={styles.container} variant="outlined">
-            <div className={styles.codeBlock}>{node.value}</div>
+            <CopyBlock
+                text={node.value}
+                language={node.lang}
+                theme={github}
+                wrapLongLines
+            />
             {node.lang && <div className={styles.lang}>{node.lang}</div>}
         </Paper>
     );
