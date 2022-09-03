fcea5204c91582ae8e8ef3efd67042d50064244e
zhongbr <1178395080@qq.com>
2022-09-03 22:37:13 +0800

    feat: test commit

--- packages/website/src/config/routers.tsx
+++ packages/website/src/config/routers.tsx
@@ -3,7 +3,7 @@
  * @Author: 张盼宏
  * @Date: 2022-08-27 23:30:19
  * @LastEditors: 张盼宏
- * @LastEditTime: 2022-09-03 22:33:52
+ * @LastEditTime: 2022-09-03 22:37:00
  */
 import { lazy } from 'react';
 import { RouteObject } from 'react-router-dom';
