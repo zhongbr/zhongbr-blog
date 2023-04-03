2080214ae85a43d2139d28a035c058c497c5fefd
zhongbr <1178395080@qq.com>
2023-04-03 10:02:32 +0800

    feat: 修改 demo 代码

--- packages/code-sandbox/src/default.ts
+++ packages/code-sandbox/src/default.ts
@@ -1,43 +1,45 @@
 export const DefaultDemoFileName = '/Demo.js';
 export const DefaultDemoCode =
-`// ⚠️require function is asynchronous !
-const React = await require('react');
-
-// if you register \`EsmToAmdPlugin\`, following statements will be support
+`// ⚠️这里这个 require 函数和标准 amd 的 require 不同，返回 promise 可以直接 await
+// const React = await require('react');
 // import React from 'react';
 
-const App = () => {
-    // if you register \`JsxPlugin\`, jsx can be used.
-    // return <div>hello world</div>;
-    return (
-        React.createElement(
-            'div',
-            {},
-            React.createElement(
-                'div',
-                {
-                    className: 'title'
-                },
-                'DemoSandbox'
-            ),
-            'welcome to use code sandbox'
-        )
-    );
-};
+// 初始化 sandbox 时注册添加 \`EsmToAmdPlugin\`, 可以使用 import 关键字
+import React, { useState } from 'react';
+// 可以直接从 unpkg.com 自动导入满足 umd / amd 规范的依赖
+import { Button, Modal } from 'antd';
 
-module.exports.default = App
-// if you register \`EsmToAmdPlugin\`, following statements will be support
-// export default App;`;
+const App: React.FC<{}> = props => {
+    const [show, setShow] = useState(false);
+    
+    return (
+        <>
+            <Modal
+                open={show}
+                title="这是一个 ant-design 的弹窗"
+                onCancel={() => setShow(false)}
+                onOk={() => setShow(false)}
+            >
+                <span>这是弹窗的内容 🚀</span>
+            </Modal>
+            <Button type="primary" onClick={() => setShow(true)}>点击Antd按钮</Button>
+        </>
+    )
+}
+
+// module.exports.default = App
+// 初始化 sandbox 时注册添加 \`EsmToAmdPlugin\`, 可以使用 export 关键字导出组件
+export default App;`;
 
 export const DefaultIndexCode =
-`const React = await require('react');
-const ReactDom = await require('react-dom');
-const App = (await require('./Demo.js')).default;
+`// const React = await require('react');
+// const ReactDom = await require('react-dom');
+// const App = (await require('./Demo.js')).default;
 
-// if you register \`EsmToAmdPlugin\`, following statements will be support
-// import React from 'react';
-// import ReactDom from 'react-dom';
-// import App from './App';
+// 初始化 sandbox 时注册添加 \`EsmToAmdPlugin\`, 可以使用 import 关键字
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App';
 
 ReactDom.render(React.createElement(App, {}), document.getElementById('root'));`;
 
