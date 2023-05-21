f162324a0414975987a4895079d394e3cc84e245
zhongbr <zhongbr@users.noreply.github.com>
2023-05-21 14:52:16 +0000

    deploy: 54db5e18429b84294f05d5b2bef5cb3004f151bb

---
+++ _next/static/TmtqMjfBkQdc_PAffEnMl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-bf2c8f512fd44040.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
